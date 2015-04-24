00aac532f8c21e965346f21dfcbd85e5bc39e892
Raul Ramirez <raul287986@gmail.com>
2015-04-23 20:22:11 -0700

    video 18 again

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	<title>My to-do list</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
 </head>
 <body>
 	 <div class="wrap">
@@ -13,15 +15,15 @@
 					$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 					$query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
 					if($result = $mysqli->query($query)){
-						$numrows = $result->num_rows:
+						$numrows = $result->num_rows;
 						if ($numrows>0) {
-							while($row = $result->fecth_assoc()){
+							while($row = $result->fetch_assoc()){
 								$task_id = $row['id'];
 								$task_name = $row["task"];
 
 								echo '<li>
-								<span>'.$task_name'</span>
-								<img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg"/>
+								<span>'.$task_name. '</span>
+								<img id="'.$task_id.'" class="delete-button" width="10px" src="images/close(1).svg"/>
 								</li>';
 								
 							}
@@ -37,7 +39,7 @@
 		</form>
 		</div>
 </body>
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
 <script>
 	add_task(); //calling the add task function
 
@@ -46,7 +48,7 @@
 			var new_task = $('.add-new-task input[name=new-task').val();
 
 			if(new_task != '') {
-				$.post('includes/addtask.php', {task: new_task}, function(data){
+				$.post('includes/add-task.php', {task: new_task}, function(data){
 					$('add-new-task input[name=new-task]').val();
 						$(data).appendTo('task-list ul').hide().fadeIn();
 				});
@@ -57,7 +59,7 @@
 
 	$('.delete-button').click(function(){
 		var current_element = $(this);
-		var task_id = $(this)attr('id');
+		var task_id = $(this).attr('id');
 
 		$.post('includes/delete-task.php', {id: task_id}, function(){
 		current_element.parent().fadeOut("fast", function(){
